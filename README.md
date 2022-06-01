7f500b71ebd05ec65494137b3c9f029ff1c0269f
Albert Bagov <albert.hall.799@gmail.com>
2022-06-01 09:52:22 +0300

    init project

---
+++ README.md
@@ -0,0 +1,4 @@
+1.  git status
+2.  git add file1 file2 file3  - добавляет файлы в Stage (подготовка к Записи, еще не сама запись=коммит)
+2.1 git add . - точка добавляет сразу все файлы
+3.  git commit -m "comment" c
---
+++ app.js
@@ -0,0 +1,39 @@
+//let userAge = 20;
+
+// Тернарный  оператор
+/* (userAge > 5 && userAge < 17) ? console.log('User ходит в школу') : alert('тернарный оператор - это замена именно для if else, а не просто if. Так как после : , которое является аналогом else, обязательно должна идти некая операция');
+ */
+
+// let favoriteDrink = 'water';
+
+// const message = favoriteDrink === 'coffee'
+//     ? 'Ваш любимый напиток - это кофе'
+//     : 'Ваш любимый напиток скорее всего чай';
+
+// console.log('message', message);
+
+/* const deadPeople = ['Lucas', 'Bill'];
+const viewedPeople = ['Jacob', 'Bill', 'Lucas'];
+const key = viewedPeople.includes(deadPeople[2]);
+console.log(key)
+*/
+
+let obj1 = { name: "Muhadin" };
+obj1.age = 37;
+console.error('obj1:', obj1)
+console.log('obj1:', obj1)
+
+// let ages = [
+//   { name: "Madina", age: 12 },
+//   { name: "Albert", age: 32 },
+//   { name: "Boris", age: 33 },
+//   { name: "Murad", age: 16 },
+//   { name: "Oleg", age: 40 },
+// ];
+
+// ages = ages.filter(checkAdult); // Returns [32, 33, 40]
+// console.log(ages);
+
+// function checkAdult(item) {
+//   return item.age >= 18;
+// }
